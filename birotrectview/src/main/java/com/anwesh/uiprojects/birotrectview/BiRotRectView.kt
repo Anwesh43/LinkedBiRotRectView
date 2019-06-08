48d1b8ba9b4398e806ab96ed24b1c16ca27fe240
Anwesh43 <anweshthecool0@gmail.com>
2019-06-08 22:25:24 +0530

    adding birotrectview class

--- birotrectview/src/main/java/com/anwesh/uiprojects/birotrectview/BiRotRectView.kt
+++ birotrectview/src/main/java/com/anwesh/uiprojects/birotrectview/BiRotRectView.kt
@@ -59,3 +59,21 @@ fun Canvas.drawBRRNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class BiRotRectView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
