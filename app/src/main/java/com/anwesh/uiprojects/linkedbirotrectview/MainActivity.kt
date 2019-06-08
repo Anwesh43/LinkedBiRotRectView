a5022e20eae484c8cd059206bb1708e1f56859df
Anwesh43 <anweshthecool0@gmail.com>
2019-06-09 00:34:40 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedbirotrectview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedbirotrectview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedbirotrectview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.birotrectview.BiRotRectView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         BiRotRectView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
