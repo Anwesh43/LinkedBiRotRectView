41d9f10e6034f9417b97348fe7f15ea1c9a94bfd
Anwesh43 <anweshthecool0@gmail.com>
2019-06-09 00:12:03 +0530

    adding BRRNode class

--- birotrectview/src/main/java/com/anwesh/uiprojects/birotrectview/BiRotRectView.kt
+++ birotrectview/src/main/java/com/anwesh/uiprojects/birotrectview/BiRotRectView.kt
@@ -124,4 +124,48 @@ class BiRotRectView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BRRNode(var i : Int, val state : State = State()) {
+
+        private var next : BRRNode? = null
+        private var prev : BRRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = BRRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBRRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BRRNode {
+            var curr : BRRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+
+    }
 }
